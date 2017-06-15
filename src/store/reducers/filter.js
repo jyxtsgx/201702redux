46cb870102f65112481c13f2f0446e785a7f036c
zhufengnodejs <zhufengnodejs@126.com>
2017-06-15 10:28:13 +0800

    init

--- src/store/reducers/filter.js
+++ src/store/reducers/filter.js
@@ -1,3 +1,10 @@
-/**
- * Created by Administrator on 2017/6/15.
- */
+import * as actionTypes from '../action-types';
+export default  function(state={},action){
+    switch (action.type){
+        //改变过滤器的类型
+        case actionTypes.CHANGE_FILTER:
+            return action.filter;
+        default:
+            return state;
+    }
+}
