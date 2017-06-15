ffeef4e0fe02a4071d55551503c82ac682bb276a
zhufengnodejs <zhufengnodejs@126.com>
2017-06-15 16:27:00 +0800

    切换完成状态

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,27 @@
 import React,{Component} from 'react';
-export default class Counter extends Component{
+import * as actionTypes from '../store/action-types';
+import {connect} from 'react-redux';
+/**
+ * 1. UI组件 木偶 傻瓜组件
+ * 2. 没有状态
+ * 3. 数据都来自于属性
+ */
+class Counter extends Component{
     render(){
         return (
             <div>
-                Counter
+                <p>{this.props.number}</p>
+                <button onClick={()=>this.props.add()}>+</button>
+                <button onClick={()=>this.props.sub()}>-</button>
             </div>
         )
     }
 }
+//把状态映射为属性对象
+let mapStateToProps = state=>({number:state})
+//把dispatch方法映射为属性对象
+let mapDispatchToProps = dispatch=>({
+    add:()=>dispatch({type:actionTypes.ADD}),
+    sub:()=>dispatch({type:actionTypes.SUB})
+})
+export default  connect(mapStateToProps,mapDispatchToProps)(Counter);
