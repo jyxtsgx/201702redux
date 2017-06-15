4243bf9e14b3948951807d10edc87b9ae3e2e9f3
zhufengnodejs <zhufengnodejs@126.com>
2017-06-15 15:53:56 +0800

    切换完成状态

---
+++ src-3/index.js
@@ -0,0 +1,7 @@
+/**
+ *上下文环境 context
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageBox from './components/MessageBox';
+ReactDOM.render(<MessageBox/>,document.querySelector('#root'));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/**
- *上下文环境 context
- */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessageBox from './components/MessageBox';
-ReactDOM.render(<MessageBox/>,document.querySelector('#root'));
+import Counter from './components/Counter';
+ReactDOM.render(<Counter/>,document.querySelector('#root'));
