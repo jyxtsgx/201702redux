dd857372e5307006756bb78c046dcc9a3a86ec1a
zhufengnodejs <zhufengnodejs@126.com>
2017-06-15 10:46:25 +0800

    init

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,34 @@
 import React,{Component} from 'react';
-export default class Todos extends Component{
+import * as actionTypes from '../store/action-types';
+//connect 连接自己的UI组件和redux store
+import {connect} from 'react-redux';
+class Todos extends Component{
     render(){
         return (
             <div style={{border:'5px solid red'}}>
                 <input type="text"/>
                 <ul>
-
+                    {
+                        this.props.todos.map((todo,index)=><li key={index}>{todo.content}</li>)
+                    }
                 </ul>
             </div>
         )
     }
 }
+//connect有两个参数 一个负责输入 一个负责输出
+//把store的状态对象映射为当前组件属性 参数是state tree
+//返回值是传入此组件的属性对象
+let mapStateToProps = state=>(
+    {todos:state.todos}
+)
+//输出
+let mapDispatchToProps = dispatch => (
+    {
+        addTodo:(content)=>dispatch({type:actionTypes.ADD_TODO,content})
+    }
+)
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Todos);
