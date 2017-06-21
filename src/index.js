5da366a5ced11319a1e861c4941208f90a5f73c1
zhufengnodejs <zhufengnodejs@126.com>
2017-06-21 12:23:20 +0800

    运行代码

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './components/Counter';
-import store from './store';
-import {Provider} from './react-redux';
-ReactDOM.render(<Provider store={store}>
-    <Counter/>
-</Provider>,document.querySelector('#root'));
+import Counter from './Counter';
+ReactDOM.render(<Counter/>,document.querySelector('#root'));
